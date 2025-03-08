a763d8d9f0a20ddb305ac6a69508c7b24bcb70bb
PAnand007 <143879416+PAnand007@users.noreply.github.com>
2025-03-08 21:45:52 +0530

    sorting bubble ,selection InsertionSort

--- DSA/InsertionSort/usingFunction.cpp
+++ DSA/InsertionSort/usingFunction.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 
 void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+  
+    
 }
 
 void printArray(int arr[], int n) {
