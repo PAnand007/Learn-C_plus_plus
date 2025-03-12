bcd1ba6fee8b7a0c534455daf5210d5b18167484
PAnand007 <143879416+PAnand007@users.noreply.github.com>
2025-03-12 23:34:17 +0530

    leetcode

---
+++ DSA/BinarySearch/leet.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    //search in roteted array 
+    // code 33
+
+    //code 658
+    //Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+    
+
+    return 0;
+}
